431831442bd7523941ac93aa8dd10e2539ff4697
nadjarutsch <nadja.rutsch@gmx.de>
2024-05-22 17:43:16 +0200

    script to copy results

--- process/copy_results.py
+++ process/copy_results.py
@@ -8,8 +8,6 @@ directory = "/gpfs/home5/nrutsch/InvalidAdjustmentSets/jobs/"
 # Read all .out files
 out_files = glob.glob(directory + "*.out")
 
-print(out_files)
-
 # Initialize a set to store unique paths
 unique_paths = set()
 
@@ -20,6 +18,7 @@ path_pattern = re.compile(r'/gpfs/scratch\d+/nodespecific/tcn\d+/nrutsch\.\d+/In
 for file in out_files:
     with open(file, 'r') as f:
         content = f.read()
+        print(content)
         paths = path_pattern.findall(content)
         unique_paths.update(paths)
 
